0f9cfc2e9cf648f53db889ca86d5a3ba5987e7d9
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-07-13 16:57:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,35 +4,36 @@
 console.log("Hello Winc Academy");
 let firstName = "Jennifer";
 console.log(firstName);
-
+        
 
 {/* In JavaScript kun je twee cijfers bij elkaar optellen. */}
-var sum = 4 + 4;
+let sum = 4 + 4;
 console.log(sum);
+        
 
-
-{/* Bij een string, telt JS de cijfers niet bij elkaar op, maar plakt de cijfers aan elkaar vast. */}
-var sumstring = "4" + "4";
+{/* Bij een string, telt JS de cijfers niet bij elkaar op,
+maar plakt de cijfers aan elkaar vast. */}
+let sumstring = "4" + "4";
 console.log(sumstring);
-
+       
 
 {/* Subtraction */}
-var difference = 45 - 33;
+let difference = 45 - 33;
 console.log(difference);
 
 
 {/* Multiplication */}
-var product = 8 * 10;
+let product = 8 * 10;
 console.log(product);
 
 
 {/* Divide */}
-var quotient = 66 / 33;
+let quotient = 66 / 33;
 console.log(quotient);
 
-
+        
 {/* Modulus */}
-var remainder = 11 % 4;
+let remainder = 11 % 4;
 console.log(remainder);
 
 
@@ -41,8 +42,8 @@ let age = 42;
 console.log(typeof age);
 
 
- {/* typeof string */}
- let ageString = "42";
-console.log(typeof ageString);
+{/* typeof string */}
+let ageString = "42";
+console.log(typeof ageString);             
 
 </script>
