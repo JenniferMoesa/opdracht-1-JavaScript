ae802625e1408ea71bd5aa6851d5f51d61a95ffd
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-07-13 16:47:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+<script>
+
+{/* Een variabele maken met if kan niet. */}
+console.log("Hello Winc Academy");
+let firstName = "Jennifer";
+console.log(firstName);
+
+
+{/* In JavaScript kun je twee cijfers bij elkaar optellen. */}
+var sum = 4 + 4;
+console.log(sum);
+
+
+{/* Bij een string, telt JS de cijfers niet bij elkaar op, maar plakt de cijfers aan elkaar vast. */}
+var sumstring = "4" + "4";
+console.log(sumstring);
+
+
+{/* Subtraction */}
+var difference = 45 - 33;
+console.log(difference);
+
+
+{/* Multiplication */}
+var product = 8 * 10;
+console.log(product);
+
+
+{/* Divide */}
+var quotient = 66 / 33;
+console.log(quotient);
+
+
+{/* Modulus */}
+var remainder = 11 % 4;
+console.log(remainder);
+
+
+{/* typeof number */}
+let age = 42;
+console.log(typeof age);
+
+
+ {/* typeof string */}
+ let ageString = "42";
+console.log(typeof ageString);
+
+</script>
